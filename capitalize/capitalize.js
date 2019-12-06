c0f706a0c92647424c76634a00d037c40a24215c
AnthonyTC89 <ptonyptc@gmail.com>
2019-12-06 17:24:00 -0500

    some linter errors solved

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const capitalize = (text) => {
   const arr = text.split('');
   arr[0] = arr[0].toUpperCase();
--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,4 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
 const capitalize = require('./capitalize');
+
 test('capitalize string', () => {
   expect(capitalize('hello world')).toBe('Hello world');
 });
