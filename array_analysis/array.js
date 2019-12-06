a202fedb472130d010165c2e52eedfbaaa28bc1d
Redvan <redvanisation@gmail.com>
2019-12-06 17:15:14 -0500

    Add array analysis Max and Len, Add linter files

--- array_analysis/array.js
+++ array_analysis/array.js
@@ -1,6 +1,10 @@
+/* eslint-disable linebreak-style */
+
 const analysis = {
-  average:  (arr) => arr.reduce((a,b) => a + b, 0) / arr.length,
-  min:      (arr) => Math.min(...arr)
-}
+  average: (arr) => arr.reduce((a, b) => a + b, 0) / arr.length,
+  min: (arr) => Math.min(...arr),
+  max: (arr) => Math.max(...arr),
+  len: (arr) => arr.length,
+};
 
 module.exports = analysis;
